80e63b9ab0cbdaf41129d35ef7caf64d36a1f259
Seliwange <yul.yaremchuk@gmail.com>
2021-12-10 22:27:35 +0200

    Updates

--- src/components/AuthorList/AuthorList.js
+++
@@ -1,9 +0,0 @@
-export default function Options({options}){
-    return(
-        options.map((option, key) => (
-            <option key={key} value={option.name}>
-                {option.name}
-            </option>
-        ))
-    );
-}
